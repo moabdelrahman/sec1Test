edc645796337e15763d04a86db79cabcb699a918
adamadam2adams <adamadam2adams@gmail.com>
2025-03-10 17:02:30 +0200

    Original version

---
+++ mathFuncs.h
@@ -0,0 +1,3 @@
+//header of the math library
+int factorial(int n);
+int gcd(int n1, int n2);
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include "mathfuncs.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"factorial of 5 =" << factorial(5)<<endl;
+    cout<<"GCD (HCF) between 48, 36 is "<<gcd(48,36);
+    return 0;
+}
